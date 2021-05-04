a62a2f49a9147e6fdc872efe7b7c2add0d0f2a68
Harlan T Wood <code@harlantwood.net>
2021-05-03 21:49:05 -1000

    Script to start n hc nodes and expose via socat

---
+++ bin/dev.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Usage:
+# bin/dev.sh [number of sandboxes]
+#
+# this will start hc nodes on ports 8000, 8001, 8002...
+# and expose since hc binds to localhost,
+# we expose them via socat on 9000, 9001, 9002...
+
+set -euo pipefail
+
+# example:
+# die "exit with this error message"
+die() { echo "$*" 1>&2 ; exit 1; }
+
+which hc || die "ERROR: executable 'hc' not found in PATH: $PATH"
+which socat || die "ERROR: executable 'socat' not found in PATH: $PATH"
+
+# use the nuclear option:
+killall hc || true
+killall socat || true
+
+sandboxes=${1:-2}  # default to 2
+port_increment=1
+min_port=8000
+max_port=$(( min_port + (sandboxes - 1) * port_increment ))
+
+for localhost_port in $(seq $min_port $port_increment $max_port); do
+  exposed_port=$(( localhost_port + 1000 ))
+  ( set -x && socat TCP-LISTEN:${exposed_port},fork,reuseaddr TCP:127.0.0.1:${localhost_port} & )
+done
+
+ports=$(seq -s , $min_port $port_increment $max_port)
+set -x
+hc sandbox generate \
+  --run=${ports} \
+  --app-id syn \
+  --num-sandboxes ${sandboxes} \
+  --root tmp \
+  network \
+    --bootstrap https://bootstrap-staging.holo.host \
+    quic
